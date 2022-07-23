2c186f1eb8123cbccebfc0c9c20579ec99f76177
MRiabov <maksymriabov2004@gmail.com>
2022-07-23 18:39:57 +0100

    Well, saturday is definitely not a weekend in my eyes.

--- src/main/java/edu/mriabov/springuniversity/controller/HolidaysController.java
+++ src/main/java/edu/mriabov/springuniversity/controller/HolidaysController.java
@@ -4,6 +4,7 @@ import edu.mriabov.springuniversity.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,17 +14,25 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(Model model) {
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model) {
+        switch (display) {
+            case "federal" -> model.addAttribute("federal",true);
+            case "festival" -> model.addAttribute("festival",true);
+            default -> {
+                model.addAttribute("federal",true);
+                model.addAttribute("festival",true);
+            }
+        }
         List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+                new Holiday(" Jan 1 ", "New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ", "Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ", "Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ", "Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ", "Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ", "Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ", "Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ", "Veterans Day", Holiday.Type.FEDERAL)
         );
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
