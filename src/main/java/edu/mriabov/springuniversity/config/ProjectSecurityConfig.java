77409289ebf851b9052b4e16a45370984c4d7d69
MRiabov <maksymriabov2004@gmail.com>
2022-07-26 20:32:20 +0100

    Implemented login, logout, dashboard. Damn, what have I not implemented yet?

--- src/main/java/edu/mriabov/springuniversity/config/ProjectSecurityConfig.java
+++ src/main/java/edu/mriabov/springuniversity/config/ProjectSecurityConfig.java
@@ -3,7 +3,9 @@ package edu.mriabov.springuniversity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +13,26 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth
-                .antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .antMatchers("/notices", "/contact").permitAll()
+        http.authorizeHttpRequests((auth) -> {try {auth
+                                .antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                                .antMatchers("/notices", "/contact").permitAll()
+                                .and().formLogin().loginPage("/login")
+                                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
         ).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("54321").roles("ADMIN")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+
     }
 }
