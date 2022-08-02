d45ab40c1e043f5788d8edbb727375314ab8438a
MRiabov <maksymriabov2004@gmail.com>
2022-08-02 15:35:43 +0100

    Added logging.

---
+++ src/main/java/edu/mriabov/springuniversity/aspects/LoggerAspect.java
@@ -0,0 +1,32 @@
+package edu.mriabov.springuniversity.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspect {
+    @Around("execution(* edu.mriabov.springuniversity..*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable{
+        log.info(joinPoint.getSignature().toString() + " method execution start");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        log.info("Time to execute "+ joinPoint.getSignature().toString() + ": " + Duration.between(Instant.now(), start));
+        log.info(joinPoint.getSignature().toString()+" method executed");
+        return result;
+    }
+
+    @AfterThrowing(value = "execution(* edu.mriabov.springuniversity.*.*(..))", throwing = "ex")
+    public void logError(JoinPoint joinPoint, Exception ex){
+        log.error(joinPoint.getSignature()+ " threw "+ex.getMessage());
+    }
+}
