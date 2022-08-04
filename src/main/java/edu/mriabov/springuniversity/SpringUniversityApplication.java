5923bb346ba7b9afec7b5683bdd0d6ebab691580
MRiabov <maksymriabov2004@gmail.com>
2022-08-04 16:57:02 +0100

    Migrating from JDBS to to JPA. And JPA is damn poggers, bruh.

--- src/main/java/edu/mriabov/springuniversity/SpringUniversityApplication.java
+++ src/main/java/edu/mriabov/springuniversity/SpringUniversityApplication.java
@@ -2,11 +2,12 @@ package edu.mriabov.springuniversity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("edu.mriabov.springuniversity.repository")
-@org.springframework.boot.autoconfigure.domain.EntityScan("edu.mriabov.springuniversity.model")
+@EntityScan("edu.mriabov.springuniversity.model")
 public class SpringUniversityApplication {
 
     public static void main(String[] args) {
