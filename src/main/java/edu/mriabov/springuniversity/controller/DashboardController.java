d8d80d04f875874c6bbe14ed20db0fff2221058d
MRiabov <maksymriabov2004@gmail.com>
2022-08-15 22:04:00 +0100

    Added logging into file.

--- src/main/java/edu/mriabov/springuniversity/controller/DashboardController.java
+++ src/main/java/edu/mriabov/springuniversity/controller/DashboardController.java
@@ -4,6 +4,7 @@ import edu.mriabov.springuniversity.model.Person;
 import edu.mriabov.springuniversity.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,16 +18,24 @@ import javax.servlet.http.HttpSession;
 public class DashboardController {
 
     final PersonRepository personRepository;
+    final Environment environment;
 
-    @RequestMapping(value = "/dashboard")
-    public String dashboard(Model model, Authentication authentication, HttpSession httpSession) {
+
+    @RequestMapping("/dashboard")
+    public String displayDashboard(Model model,Authentication authentication, HttpSession session) {
         Person person = personRepository.readByEmail(authentication.getName());
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
         if (person.getEazyClass() != null && person.getEazyClass().getName() != null) {
             model.addAttribute("enrolledClass",person.getEazyClass());
         }
-        httpSession.setAttribute("loggedInUser", person);
+        session.setAttribute("loggedInUser", person);
+        logMessages();
         return "dashboard.html";
+    }
+    private void logMessages() {
+        log.error("defaultPageSize value with Environment is : "+environment.getProperty("springuniversity.pageSize"));
+        log.error("successMsg value with Environment is : "+environment.getProperty("springuniversity.contact.successMsg"));
+        log.error("Java Home environment variable using Environment is : "+environment.getProperty("JAVA_HOME"));
     }
 }
